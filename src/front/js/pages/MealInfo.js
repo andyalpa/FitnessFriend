cbb73cd5551e5054dd96da7a415be519cf58c78f
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-12-13 01:40:02 +0000

    Fixed the log in modal, will do the log out functionality now

--- src/front/js/pages/MealInfo.js
+++ src/front/js/pages/MealInfo.js
@@ -23,39 +23,39 @@ const MealInfo = () => {
     .filter(key => key.startsWith('strMeasure') && info[key])
     .map(key => info[key]);
 
-    const videoId = () => {
-      let vId = '';
-      if (info) {
-        const videoUrl = info.strYoutube;
-        if (videoUrl) {
-          const videoStr = videoUrl.split('=');
-          vId = videoStr[videoStr.length - 1];
-        }
+  const videoId = () => {
+    let vId = '';
+    if (info) {
+      const videoUrl = info.strYoutube;
+      if (videoUrl) {
+        const videoStr = videoUrl.split('=');
+        vId = videoStr[videoStr.length - 1];
       }
-      return vId;
-    };
-  
-  
-    const vId = videoId()
-  
+    }
+    return vId;
+  };
+
+
+  const vId = videoId()
+
   return (
     <>
       {!info ? (
         ""
       ) : (
-        <div data-aos="fade-in" className="recipe-box card mb-3 m-5" style={{ minWidth: "540px", borderRadius: "1.25rem", boxShadow: "0px 0px 30px 7px rgba(0,0,0,0.1)"}}>
-          <div className="" style={{ flexDirection: "row-reverse"}}>
-            <div class="meal-info_header d-flex">
+        <div data-aos="fade-in" className="recipe-box card mb-3 m-5" style={{ minWidth: "540px", borderRadius: "1.25rem", boxShadow: "0px 0px 30px 7px rgba(0,0,0,0.1)" }}>
+          <div className="" style={{ flexDirection: "row-reverse" }}>
+            <div className="meal-info_header d-flex">
               <div data-aos="fade-up-left" style={{ "border-radius": "20px !important" }} >
                 <img style={{ borderRadius: "1.25rem", width: "300px" }} className="img-fluid" src={info.strMealThumb} alt={info.strMeal} />
               </div>
-              <h1 data-aos="fade-down-right" style={{alignSelf: "center", fontSize: "2rem", marginLeft: "3rem"}}>{info.strMeal}</h1>
+              <h1 data-aos="fade-down-right" style={{ alignSelf: "center", fontSize: "2rem", marginLeft: "3rem" }}>{info.strMeal}</h1>
             </div>
 
             <div style={{ display: "flex", flexDirection: "column" }}>
-              
-              
-              <div class="meal-body my-3">
+
+
+              <div className="meal-body my-3">
                 <div data-aos="fade-right" class="meal-ingredients">
                   <h2>Ingredients</h2>
                   <ul className="list-group list-group-vertical">
@@ -73,8 +73,8 @@ const MealInfo = () => {
             </div>
           </div>
           <div data-aos="zoom-out-up">
-          <h2>Video Tutorial</h2>
-            <div style={{height: "499px"}}  className="tutorialVideo corner-wrapper ">
+            <h2>Video Tutorial</h2>
+            <div style={{ height: "499px" }} className="tutorialVideo corner-wrapper ">
               <iframe src={`https://www.youtube.com/embed/${vId}`}
                 title="Youtube video" allowFullScreen>
               </iframe>
